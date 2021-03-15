957a3414070c4da59e8cf8fab98e3eaf56e0e492
yxun <yuanlin.yxu@gmail.com>
2021-03-14 23:09:44 -0400

    Add building native image scripts

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,7 +2,35 @@
 
 PROJECT=getting-started
 
-echo "Packaging..."
-pushd ../${PROJECT}
-./mvnw package
-popd
+function package() {
+    echo "Packaging..."
+    pushd ../${PROJECT}
+    ./mvnw package
+    popd
+}
+
+function package_native() {
+    echo "Packaging..."
+    pushd ../${PROJECT}
+    ./mvnw package -Pnative
+    # ./target/${PROJECT}-1.0.0-SNAPSHOT-runner
+    ./mvnw verify -Pnative
+    popd
+}
+
+function package_container() {
+    echo "Packaging..."
+    pushd ../${PROJECT}
+    ./mvnw package -Pnative \
+    -Dquarkus.native.container-build=true \
+    -Dquarkus.container-image.build=true
+    # application.properties
+    # Dquarkus.native.container-runtime=docker or podman
+    popd
+
+    # manually build docker image
+    # edit .dockerignore and remove or comment its content
+    # docker build -f src/main/docker/Dockerfile.native -t quarkus-quickstart/getting-started .
+    # docker run -i --rm -p 8080:8080 quarkus-quickstart/getting-started
+
+}
